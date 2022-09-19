3cfa4c0c154002f9f0c963997f332a6875419588
sklohrm <sklohrm@gmail.com>
2022-09-18 20:36:54 -0500

    Initial Commit

---
+++ DotaKeeper/DotaKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotaKeeperApp.swift
+//  DotaKeeper
+//
+//  Created by Spencer Lohrmann on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct DotaKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
